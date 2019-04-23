fce67473ed00730dc3216a0e85841c87c88fbeaf
Kelly Yi <theguykelly@gmail.com>
2019-04-22 18:53:26 -0700

    starting to add about me redirect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import About from "./pages/About";
+import Navbar from "./pages/Navbar";
+import PageLoader from "./pages/PageLoader";
+import { Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<PageLoader />}>
+      <Route path="/" component={Navbar} />
+      <Route path="/about" component={About} />
+    </Suspense>
   );
 }
 
---
+++ src/pages/PageLoader.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const PageLoader = () => {
+    return(
+        <div>
+            <h1>THIS IS THE PAGE LOADER LOADING...</h1></div>
+    )
+}
+
+export default PageLoader;
